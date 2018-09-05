6e3ae3e27e2dee4150c45b6526c92dde89d77346
kurbatsky <kurbatskykv@gmail.com>
2018-09-05 22:12:58 +0300

    commit 1 Превью готово

---
+++ PictureViewer/Models/Picture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+using System.IO;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Controls;
+namespace PictureViewer
+{
+    class Picture
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Path_to_Pic
+        {
+            get;
+            set;
+        }
+        public double Size
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ PictureViewer/ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+using System.IO;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Controls;
+namespace PictureViewer
+{
+    class ViewModel : ViewModelBase
+    {
+        List<string> filter = new List<string>() { @"bmp", @"jpg", @"gif", @"png", @"ico", @"jpeg" };
+        public ObservableCollection<Picture> Images
+        {
+            get;
+            private set;
+        }
+        public ViewModel()
+        {
+            Images = new ObservableCollection<Picture>();
+            
+        }
+        
+        #region Commands
+        ICommand _get_images;
+        public ICommand Get_images
+        {
+            get
+            {
+                return _get_images ?? (_get_images = new RelayCommand(Load_files));
+            }
+        }
+
+        #endregion
+        void Load_files()
+        {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Images.Clear();
+                foreach (string file in Directory.GetFiles(dialog.SelectedPath))
+                {
+                    if (filter.Exists(n => n == file.Split(new char[] { '.' }).Last().ToLower()))
+                    {
+                        try
+                        {
+                            var fileinfo = new FileInfo(file);
+                            Images.Add(new Picture {Name = fileinfo.Name, Path_to_Pic = fileinfo.FullName,Size =fileinfo.Length/1000});
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                   
+            }
+        }
+    }
+}
